b18e6f46c7dacccbd7645d067f73cf8deaddffa0
Gram <master_fess@mail.ru>
2020-10-21 13:21:26 +0200

    second try on golangci-lint

--- wasm/linter.go
+++ wasm/linter.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
 	"context"
 
 	"github.com/golangci/golangci-lint/pkg/config"
-	"github.com/golangci/golangci-lint/pkg/fsutils"
-	"github.com/golangci/golangci-lint/pkg/golinters/goanalysis/load"
-	"github.com/golangci/golangci-lint/pkg/goutil"
 	"github.com/golangci/golangci-lint/pkg/lint"
 	"github.com/golangci/golangci-lint/pkg/lint/lintersdb"
 	"github.com/golangci/golangci-lint/pkg/logutils"
 	"github.com/golangci/golangci-lint/pkg/report"
 	"github.com/golangci/golangci-lint/pkg/result"
-	"github.com/golangci/golangci-lint/pkg/result/processors"
 )
 
 type Runner struct {
@@ -29,51 +22,19 @@ func RunLinters() ([]result.Issue, error) {
 	cfg := config.NewDefault()
 	reportData := report.Data{}
 	log := report.NewLogWrapper(logutils.NewMockLog(), &reportData)
-	loadGuard := load.NewGuard()
-	goenv := goutil.NewEnv(log)
-	fileCache := fsutils.NewFileCache()
-	lineCache := fsutils.NewLineCache(fileCache)
-	contextLoader := lint.NewContextLoader(cfg, log, goenv, lineCache, fileCache, nil, loadGuard)
+	DBManager := lintersdb.NewManager(cfg, log).WithCustomLinters()
+	linters := DBManager.GetAllSupportedLinterConfigs()
 
 	ctx := context.TODO()
-	ctx, cancel := context.WithTimeout(ctx, 100*time.Millisecond)
-	defer cancel()
 
-	DBManager := lintersdb.NewManager(cfg, log).WithCustomLinters()
-	validator := lintersdb.NewValidator(DBManager)
-	EnabledLintersSet := lintersdb.NewEnabledSet(DBManager, validator, log, cfg)
-
-	lintersToRun, err := EnabledLintersSet.GetOptimizedLinters()
-	if err != nil {
-		return nil, fmt.Errorf("cannot get optimized linters: %v", err)
+	issues := make([]result.Issue, 0)
+	for _, lc := range linters {
+		linterIssues, err := lc.Linter.Run(ctx, nil)
+		if err != nil {
+			return nil, err
+		}
+		issues = append(issues, linterIssues...)
 	}
 
-	enabledLintersMap, err := EnabledLintersSet.GetEnabledLintersMap()
-	if err != nil {
-		return nil, fmt.Errorf("cannot get enabled linters map: %v", err)
-	}
-
-	for _, lc := range DBManager.GetAllSupportedLinterConfigs() {
-		isEnabled := enabledLintersMap[lc.Name()] != nil
-		reportData.AddLinter(lc.Name(), isEnabled, lc.EnabledByDefault)
-	}
-
-	lintCtx, err := contextLoader.Load(ctx, lintersToRun)
-	if err != nil {
-		return nil, fmt.Errorf("context loading failed: %v", err)
-	}
-	lintCtx.Log = log
-
-	runner, err := lint.NewRunner(cfg, log,
-		goenv, EnabledLintersSet, lineCache, DBManager, lintCtx.Packages)
-	if err != nil {
-		return nil, fmt.Errorf("cannot get linters runner: %v", err)
-	}
-	issues, err := runner.Run(ctx, lintersToRun, lintCtx)
-	if err != nil {
-		return nil, fmt.Errorf("cannot run linters: %v", err)
-	}
-
-	fixer := processors.NewFixer(cfg, log, fileCache)
-	return fixer.Process(issues), nil
+	return issues, nil
 }
