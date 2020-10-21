2b87b5d8d055498d1b3b07d59cb7fd6c1f6c61d0
Gram <master_fess@mail.ru>
2020-10-21 14:50:16 +0200

    integrate ruleguard with UI

--- wasm/main.go
+++ wasm/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"syscall/js"
 
 	"github.com/life4/gweb/web"
 )
@@ -13,6 +14,33 @@ func main() {
 	fmt.Println("hello world")
 }
 `
+
+func lint(editor web.Value, doc *web.Document) {
+	out := doc.Element("lint-output")
+	rule := doc.Element("lint-rule").Get("value").String()
+	src := editor.Call("getValue").String()
+
+	issues, err := RunRuleGuard(src, rule)
+	if err != nil {
+		out.SetText(err.Error())
+		return
+	}
+	fmt.Println(issues)
+	out.SetText(fmt.Sprintln(issues))
+}
+
+func register(editor web.Value, doc *web.Document) {
+	btn := doc.Element("lint-run")
+	btn.Set("disabled", false)
+
+	wrapped := func(this js.Value, args []js.Value) interface{} {
+		btn.Set("disabled", true)
+		lint(editor, doc)
+		register(editor, doc)
+		return true
+	}
+	btn.Call("addEventListener", "click", js.FuncOf(wrapped))
+}
 
 func main() {
 	window := web.GetWindow()
@@ -28,13 +56,6 @@ func main() {
 			"lineNumbers": true,
 		})
 
-	// run linter
-	src := editor.Call("getValue").String()
-	rule := "m.Match(`$x`)"
-	issues, err := RunRuleGuard(src, rule)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(issues)
+	register(editor, &doc)
 	select {}
 }
