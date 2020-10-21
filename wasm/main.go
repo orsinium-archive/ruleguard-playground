c5c17353fde1903a3078de6bbeeb61669e26e916
Gram <master_fess@mail.ru>
2020-10-21 15:15:38 +0200

    show better position

--- wasm/main.go
+++ wasm/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"syscall/js"
-
 	"github.com/life4/gweb/web"
 )
 
@@ -14,33 +11,6 @@ func main() {
 	fmt.Println("hello world")
 }
 `
-
-func lint(editor web.Value, doc *web.Document) {
-	out := doc.Element("lint-output")
-	rule := doc.Element("lint-rule").Get("value").String()
-	src := editor.Call("getValue").String()
-
-	issues, err := RunRuleGuard(src, rule)
-	if err != nil {
-		out.SetText(err.Error())
-		return
-	}
-	fmt.Println(issues)
-	out.SetText(fmt.Sprintln(issues))
-}
-
-func register(editor web.Value, doc *web.Document) {
-	btn := doc.Element("lint-run")
-	btn.Set("disabled", false)
-
-	wrapped := func(this js.Value, args []js.Value) interface{} {
-		btn.Set("disabled", true)
-		lint(editor, doc)
-		register(editor, doc)
-		return true
-	}
-	btn.Call("addEventListener", "click", js.FuncOf(wrapped))
-}
 
 func main() {
 	window := web.GetWindow()
@@ -56,6 +26,7 @@ func main() {
 			"lineNumbers": true,
 		})
 
-	register(editor, &doc)
+	rg := NewRuleGuard(editor, &doc)
+	rg.Register()
 	select {}
 }
--- wasm/ruleguard.go
+++ wasm/ruleguard.go
@@ -1,20 +1,72 @@
 package main
 
 import (
+	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
 	"go/types"
+	"syscall/js"
 
+	"github.com/life4/gweb/web"
 	"github.com/quasilyte/go-ruleguard/analyzer"
 	"golang.org/x/tools/go/analysis"
 )
 
-func RunRuleGuard(src string, rule string) ([]analysis.Diagnostic, error) {
-	const filename = "main.go"
-	const mode = parser.AllErrors | parser.ParseComments
+const filename = "main.go"
+const mode = parser.AllErrors | parser.ParseComments
 
-	issues := make([]analysis.Diagnostic, 0)
+type RuleGuard struct {
+	out    web.HTMLElement
+	editor web.Value
+	doc    *web.Document
+}
+
+func NewRuleGuard(editor web.Value, doc *web.Document) RuleGuard {
+	return RuleGuard{
+		editor: editor,
+		doc:    doc,
+		out:    doc.Element("lint-output"),
+	}
+}
+
+func (rg *RuleGuard) Register() {
+	btn := rg.doc.Element("lint-run")
+	btn.Set("disabled", false)
+
+	wrapped := func(this js.Value, args []js.Value) interface{} {
+		btn.Set("disabled", true)
+		rg.RunAndPrint()
+		rg.Register()
+		return true
+	}
+	btn.Call("addEventListener", "click", js.FuncOf(wrapped))
+}
+
+func (rg *RuleGuard) RunAndPrint() {
+	rg.out.SetText("Running...")
+	rule := rg.doc.Element("lint-rule").Get("value").String()
+	src := rg.editor.Call("getValue").String()
+
+	// parse source code
+	pass, err := rg.makePass(src)
+	if err != nil {
+		rg.out.SetText(err.Error())
+		return
+	}
+
+	// run linter
+	issues, err := rg.Run(pass, src, rule)
+	if err != nil {
+		rg.out.SetText(err.Error())
+		return
+	}
+
+	// print violations
+	rg.table(issues, pass)
+}
+
+func (rg *RuleGuard) makePass(src string) (*analysis.Pass, error) {
 	fset := token.NewFileSet()
 	tree, err := parser.ParseFile(fset, filename, src, mode)
 	if err != nil {
@@ -24,16 +76,59 @@ func RunRuleGuard(src string, rule string) ([]analysis.Diagnostic, error) {
 		Fset:  fset,
 		Files: []*ast.File{tree},
 		Pkg:   types.NewPackage("main", "main"),
-		Report: func(d analysis.Diagnostic) {
-			issues = append(issues, d)
-		},
 	}
-
+	return &pass, nil
+}
+func (rg *RuleGuard) Run(pass *analysis.Pass, src string, rule string) ([]analysis.Diagnostic, error) {
+	issues := make([]analysis.Diagnostic, 0)
+	pass.Report = func(d analysis.Diagnostic) {
+		issues = append(issues, d)
+	}
 	an := analyzer.Analyzer
-	an.Flags.Set("e", rule)
-	_, err = an.Run(&pass)
+	err := an.Flags.Set("e", rule)
+	if err != nil {
+		return nil, err
+	}
+	_, err = an.Run(pass)
 	if err != nil {
 		return nil, err
 	}
 	return issues, nil
+}
+
+func (rg *RuleGuard) table(issues []analysis.Diagnostic, pass *analysis.Pass) {
+	table := rg.doc.CreateElement("table")
+	table.Attribute("class").Set("table table-sm")
+
+	thead := rg.doc.CreateElement("thead")
+	table.Node().AppendChild(thead.Node())
+	tr := rg.doc.CreateElement("tr")
+	thead.Node().AppendChild(tr.Node())
+
+	cols := []string{"pos", "message"}
+	for _, name := range cols {
+		th := rg.doc.CreateElement("th")
+		th.SetText(name)
+		tr.Node().AppendChild(th.Node())
+	}
+
+	tbody := rg.doc.CreateElement("tbody")
+	table.Node().AppendChild(tbody.Node())
+
+	for _, issue := range issues {
+		tr := rg.doc.CreateElement("tr")
+
+		td := rg.doc.CreateElement("td")
+		pos := pass.Fset.Position(issue.Pos)
+		td.SetText(fmt.Sprintf("%d:%d", pos.Line, pos.Column))
+		tr.Node().AppendChild(td.Node())
+
+		td = rg.doc.CreateElement("td")
+		td.SetText(issue.Message)
+		tr.Node().AppendChild(td.Node())
+
+		tbody.Node().AppendChild(tr.Node())
+	}
+
+	rg.out.Node().AppendChild(table.Node())
 }
